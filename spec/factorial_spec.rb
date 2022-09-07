d5d604a0b85d4e60bf2ca417b3a56b8f5a07f396
ppjd <pauljdavi.t21@btinternet.com>
2022-09-07 11:57:34 +0100

    test code

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,6 +1,13 @@
 require "rspec/autorun"
 
-describe factorial do 
+    describe factorial do 
+        it 'finds a factorial of a number' do
+            calculator = Factorial.new
 
-end
+            expect(calculator.factorial_of(5)).to eq(120)
+        end
+    end
 
+# describe  - an example group - setup of a block
+# it - defines the test case
+# expect - step to check that a condition/expectation has been met
